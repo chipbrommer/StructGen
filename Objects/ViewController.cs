420e9e0844f49843a51935bb20f189869bb2e31d
Chip Brommer <chip.brommer@aseifl.com>
2023-09-30 12:19:00 -0500

    add view controller

---
+++ Objects/ViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructGen.Objects
+{
+    public class ViewController
+    {
+        public static ViewTypes CurrentView { get; set; }
+
+        public enum ViewTypes
+        {
+            Main,
+            Settings,
+            Document,
+            Parse,
+            Startup
+        }
+
+        public void ChangeView(ViewTypes view)
+        {
+            
+        }
+    }
+}
